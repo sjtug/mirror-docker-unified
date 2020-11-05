4522db39357a2d9c663f1fbeb321274aaa25fd5e
Alex Chi <iskyzh@gmail.com>
2020-11-05 09:24:39 +0800

    caddy: add format command

--- scripts/download_latest_frontend.sh
+++ scripts/download_latest_frontend.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-LATEST_URL=$(curl https://api.github.com/repos/sjtug/sjtug-mirror-frontend/releases/latest | jq '.assets[0].browser_download_url' -r)
+LATEST_URL=$(curl -sfL https://api.github.com/repos/sjtug/sjtug-mirror-frontend/releases/latest | jq '.assets[0].browser_download_url' -r)
 wget -N -O /tmp/dists.tar.gz "$LATEST_URL"
 tar -zxf /tmp/dists.tar.gz --directory data/caddy/dists
