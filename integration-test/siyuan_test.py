2b56f2939e32a19b960306d580b4097cbe21183f
Alex Chi <iskyzh@gmail.com>
2020-11-08 11:05:59 +0800

    integration-test: refine expected behavior (#35)
    Co-authored-by: Bugen Zhao <i@bugenzhao.com>

---
+++ integration-test/siyuan_test.py
@@ -0,0 +1,94 @@
+import requests
+
+BASE_URL = "mirrors.internal.skyzh.xyz"
+
+"""
+Here we take Debian repo as an example. It should be served on both http
+and https. Caddy should handle the redirections correctly.
+"""
+
+def test_debian_http_base():
+    response = requests.get(f"http://{BASE_URL}/debian")
+    assert response.history
+    assert response.url == f"http://{BASE_URL}/debian/"
+
+def test_debian_http_dir():
+    response = requests.get(f"http://{BASE_URL}/debian/pool")
+    assert response.history
+    assert response.url == f"http://{BASE_URL}/debian/pool/"
+
+def test_debian_https_base():
+    response = requests.get(f"https://{BASE_URL}/debian")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/debian/"
+
+def test_debian_https_dir():
+    response = requests.get(f"https://{BASE_URL}/debian/pool")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/debian/pool/"
+
+"""
+Here we take conda repo as an example. It should be served only on https
+interface. All http request should be redirected to https.
+"""
+
+""" Currently Siyuan mirror has no such entry.
+
+def test_conda_http_base():
+    response = requests.get(f"http://{BASE_URL}/anaconda")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/anaconda/"
+
+def test_conda_http_dir():
+    response = requests.get(f"http://{BASE_URL}/anaconda/cloud")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/anaconda/cloud/"
+
+def test_conda_https_base():
+    response = requests.get(f"https://{BASE_URL}/anaconda")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/anaconda/"
+
+def test_conda_https_dir():
+    response = requests.get(f"https://{BASE_URL}/anaconda/cloud")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/anaconda/cloud/"
+
+def test_conda_file():
+    response = requests.get(f"http://{BASE_URL}/anaconda/cloud/pytorch/noarch/repodata.json")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/anaconda/cloud/pytorch/noarch/repodata.json"
+"""
+
+"""
+And for our frontend, it should always be available on https.
+"""
+
+def test_frontend_base():
+    response = requests.get(f"http://{BASE_URL}")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/"
+
+def test_frontend_index():
+    response = requests.get(f"http://{BASE_URL}/index.html")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/"
+
+def test_frontend_docs():
+    response = requests.get(f"http://{BASE_URL}/docs/anaconda")
+    assert response.history
+    assert response.status_code == 200
+    assert response.url == f"https://{BASE_URL}/docs/anaconda"
+
+def test_lug_base():
+    response = requests.get(f"http://{BASE_URL}/lug/v1/manager/summary")
+    assert response.history
+    assert response.url == f"https://{BASE_URL}/lug/v1/manager/summary"
+
+"""
+Also an edge case, we should never concat two directories
+"""
+
+def test_debian_concat():
+    response = requests.get(f"http://{BASE_URL}/debiandoc/source-unpack.txt")
+    assert response.status_code == 404
