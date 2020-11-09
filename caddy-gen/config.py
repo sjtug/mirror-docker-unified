eabf6f43399249f3bbed99c47f9e6192ccc46a91
Alex Chi <iskyzh@gmail.com>
2020-11-09 14:42:50 +0800

    monitor: use host node_exporter (#58)

--- caddy-gen/config.py
+++ caddy-gen/config.py
@@ -1,5 +1,5 @@
 BASE = [':80', 'mirrors2.sjtug.sjtu.edu.cn']
 LUG_ADDR = 'siyuan-lug:7001'
 FRONTEND_DIR = '/dists'
-NODE_EXPORTER_ADDR = 'siyuan-node_exporter:9100'
+NODE_EXPORTER_ADDR = 'siyuan-node_exporter:9109'
 CADVISOR_ADDR = 'siyuan-cadvisor:8080'
