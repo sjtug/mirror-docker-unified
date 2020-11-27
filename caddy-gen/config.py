d37512e7cb33410749d43e7b0e9d47a88a2b4470
Alex Chi <iskyzh@gmail.com>
2020-11-27 18:15:56 +0800

    tunnel: add log tunnel through v2ray (#67)

--- caddy-gen/config.py
+++ caddy-gen/config.py
@@ -1,6 +1,6 @@
 BASE = [':80', 'mirrors2.sjtug.sjtu.edu.cn']
 LUG_ADDR = 'siyuan-lug:7001'
 FRONTEND_DIR = '/dists'
-NODE_EXPORTER_ADDR = 'siyuan-node_exporter:9109'
+NODE_EXPORTER_ADDR = '172.17.0.1:9100'
 CADVISOR_ADDR = 'siyuan-cadvisor:8080'
 LUG_EXPORTER_ADDR = 'siyuan-lug:8081'
