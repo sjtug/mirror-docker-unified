74842a692918d7e73875e401aad2ef2375e4903d
Alex Chi <iskyzh@gmail.com>
2020-12-03 22:11:07 +0800

    update Caddyfile

--- caddy-gen/config.py
+++ caddy-gen/config.py
@@ -4,3 +4,4 @@ FRONTEND_DIR = '/dists'
 NODE_EXPORTER_ADDR = '172.17.0.1:9100'
 CADVISOR_ADDR = 'siyuan-cadvisor:8080'
 LUG_EXPORTER_ADDR = 'siyuan-lug:8081'
+MIRROR_INTEL_LIST = ['crates.io', 'pypi-packages', 'flathub', 'fedora-ostree']
