208eaac441eda9c217b6499a1c11a499088a33b0
Alex Chi <iskyzh@gmail.com>
2020-12-15 23:15:06 +0800

    lug: add git repo

---
+++ lug/worker-script/git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [[ ! -d $LUG_path ]]; then
+    git clone --mirror $LUG_source $LUG_path
+    cd $LUG_path
+    git remote add upstream $LUG_target
+fi
+
+cd $LUG_path
+
+git remote set-url origin $LUG_source
+git remote set-url upstream $LUG_target
+
+git config --unset-all remote.origin.fetch
+git config --add remote.origin.fetch "+refs/heads/*:refs/heads/*"
+git config --add remote.origin.fetch "+refs/tags/*:refs/tags/*"
+
+# dpdk repo contains "remotes/github". We should remove them.
+git for-each-ref --format 'delete %(refname)' refs/remotes | git update-ref --stdin
+
+git fetch -p origin
+git push --mirror upstream
