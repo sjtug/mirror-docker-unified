6d2d57cf4c29aa5adc5031560781c24be4128394
Alex Chi <iskyzh@gmail.com>
2020-12-15 23:20:28 +0800

    remove pr

--- lug/worker-script/git.sh
+++ lug/worker-script/git.sh
@@ -17,8 +17,10 @@ git config --unset-all remote.origin.fetch
 git config --add remote.origin.fetch "+refs/heads/*:refs/heads/*"
 git config --add remote.origin.fetch "+refs/tags/*:refs/tags/*"
 
+git fetch -p origin
+
 # dpdk repo contains "remotes/github". We should remove them.
 git for-each-ref --format 'delete %(refname)' refs/remotes | git update-ref --stdin
+git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 
-git fetch -p origin
 git push --mirror upstream
