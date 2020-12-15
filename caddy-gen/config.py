8c7d9a585f8014c82e61b4584ce639516c8c11bd
Alex Chi <iskyzh@gmail.com>
2020-12-15 18:49:08 +0800

    mirror-intel: upgrade to 1.10 (#85)

--- caddy-gen/config.py
+++ caddy-gen/config.py
@@ -1,4 +1,4 @@
-BASE = [':80', 'mirrors2.sjtug.sjtu.edu.cn']
+BASE = ['ftp.sjtu.edu.cn', 'siyuan.internal.sjtug.org', ':80']
 LUG_ADDR = 'siyuan-lug:7001'
 FRONTEND_DIR = '/dists'
 NODE_EXPORTER_ADDR = '172.17.0.1:9100'
@@ -7,4 +7,4 @@ MIRROR_INTEL_ADDR = 'siyuan-mirror-intel:8000'
 LUG_EXPORTER_ADDR = 'siyuan-lug:8081'
 MIRROR_INTEL_LIST = ['crates.io', 'pypi-packages', 'flathub', 'fedora-ostree',
                      'fedora-iot', 'rust-static', 'homebrew-bottles', 'dart-pub', 'guix',
-                     'pytorch-wheels', 'sjtug-internal']
+                     'pytorch-wheels', 'sjtug-internal', 'flutter_infra']
