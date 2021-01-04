d999ee3ebb8c11c09dd986ecf5baaea23ba6283f
Alex Chi <iskyzh@gmail.com>
2021-01-04 13:20:08 +0800

    speed up build

--- caddy-gen/config.py
+++ caddy-gen/config.py
@@ -1,10 +1,10 @@
 BASE = ['ftp.sjtu.edu.cn', 'siyuan.internal.sjtug.org']
-LUG_ADDR = 'siyuan-lug:7001'
+LUG_ADDR = 'lug:7001'
 FRONTEND_DIR = '/dists'
 NODE_EXPORTER_ADDR = '172.31.0.1:9100'
-CADVISOR_ADDR = 'siyuan-cadvisor:8080'
-MIRROR_INTEL_ADDR = 'siyuan-mirror-intel:8000'
-LUG_EXPORTER_ADDR = 'siyuan-lug:8081'
+CADVISOR_ADDR = 'cadvisor:8080'
+MIRROR_INTEL_ADDR = 'mirror-intel:8000'
+LUG_EXPORTER_ADDR = 'lug:8081'
 MIRROR_INTEL_LIST = ['crates.io', 'pypi-packages', 'flathub', 'fedora-ostree',
                      'fedora-iot', 'rust-static', 'homebrew-bottles', 'dart-pub', 'guix',
                      'pytorch-wheels', 'sjtug-internal', 'flutter_infra', 'linuxbrew-bottles',
