f6699d6fe98ed83b5c676a4ab06dc865162502e9
Alex Chi <iskyzh@gmail.com>
2021-02-10 17:54:00 +0800

    mirror-intel: allow docker/compose

--- lug/build-script/setup-php.sh
+++ lug/build-script/setup-php.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+wget -q -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 if [ "$USE_SJTUG" = true ] ; then
     echo "deb https://mirror.sjtu.edu.cn/sury/php/ buster main" > /etc/apt/sources.list.d/php.list
 else
