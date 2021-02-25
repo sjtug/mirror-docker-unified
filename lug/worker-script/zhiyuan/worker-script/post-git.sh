0ac92a3ec593cd1ccfdf8d0c16c4b2c36df357ba
Alex Chi <iskyzh@gmail.com>
2021-02-25 10:16:34 +0800

    lug: switch all git repo self-contained (#176)

--- lug/worker-script/zhiyuan/worker-script/post-git.sh
+++ lug/worker-script/zhiyuan/worker-script/post-git.sh
@@ -5,4 +5,4 @@ set -xe
 cd "$LUG_path"
 
 git remote add upstream "$LUG_target" || git remote set-url upstream "$LUG_target"
-git push --all -f upstream
+timeout 10 git push --all -f upstream || true
