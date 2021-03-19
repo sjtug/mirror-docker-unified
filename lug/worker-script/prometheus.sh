c2519e2ecbaafef89dcafb39a43791d53d9cb479
Alex Chi <iskyzh@gmail.com>
2021-03-19 11:14:12 +0800

    lug: upgrade mirror-clone to 2.9

--- lug/worker-script/prometheus.sh
+++ lug/worker-script/prometheus.sh
@@ -3,5 +3,5 @@
 set -e
 
 for REPO in "prometheus/prometheus" "prometheus/node_exporter" "prometheus/alertmanager" "prometheus/blackbox_exporter" "prometheus/consul_exporter" "prometheus/graphite_exporter" "prometheus/haproxy_exporter" "prometheus/memcached_exporter" "prometheus/mysqld_exporter" "prometheus/pushgateway" "prometheus/statsd_exporter"; do
-/app/mirror-clone --concurrent_resolve 8 --workers 4 github_release --repo $REPO --target http://siyuan-mirror-intel:8000/github-release
+    /worker-script/github-release.sh $REPO 5
 done
