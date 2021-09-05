d3fef97ee58d0b89f2c3115bdf72aab190b5c36f
Alex Chi <iskyzh@gmail.com>
2021-09-05 10:16:35 +0800

    lug: update emacs-elpa

--- lug/worker-script/zhiyuan/worker-script/emacs-elpa.sh
+++ lug/worker-script/zhiyuan/worker-script/emacs-elpa.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $DIR/rsync.sh
 
-for value in emacswiki gnu marmalade melpa melpa-stable org SC sunrise-commander user42
+for value in gnu melpa melpa-stable org
 do
     echo "Too many files to list" > ${LUG_path}/${value}/index.html
 done
