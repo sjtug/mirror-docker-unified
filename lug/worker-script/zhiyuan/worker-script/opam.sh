f0d4fd5b4965008591b3ebe8c9d2de7578a175df
Alex Chi <iskyzh@gmail.com>
2022-02-13 20:43:41 +0800

    lug: bump yq to 4.19.1
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- lug/worker-script/zhiyuan/worker-script/opam.sh
+++ lug/worker-script/zhiyuan/worker-script/opam.sh
@@ -6,8 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/pre-git.sh
 
 pushd "$LUG_path"
-yq w --style=double repo archive-mirrors "https://mirrors.sjtug.sjtu.edu.cn/opam-cache" > repo.tmp
-mv repo.tmp repo
+yq -i '.archive-mirrors = "https://mirrors.sjtug.sjtu.edu.cn/opam-cache"' repo
 git config user.name 'SJTUG mirrors'
 git config user.email 'mirrors@sjtug.org'
 git add .
