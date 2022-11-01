d29f19344f78ecf1c681a0142c5bf7c891006233
LightQuantum <self@lightquantum.me>
2022-11-01 11:42:36 +0800

    fix(opam): repo rewriting (#344)

--- lug/worker-script/zhiyuan/worker-script/opam.sh
+++ lug/worker-script/zhiyuan/worker-script/opam.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/pre-git.sh
 
 pushd "$LUG_path"
-yq -i '.archive-mirrors = "https://mirrors.sjtug.sjtu.edu.cn/opam-cache"' repo
+echo '\narchive-mirrors = "https://mirrors.sjtug.sjtu.edu.cn/opam-cache"' >> repo
 git config user.name 'SJTUG mirrors'
 git config user.email 'mirrors@sjtug.org'
 git add .
