44e0caef2549e5698b7d13bfa3a7e6e9239c6b0e
LightQuantum <self@lightquantum.me>
2023-02-20 22:28:04 +0800

    lug: support extra flags for rsync-fetcher

--- lug/worker-script/rsync-fetcher.sh
+++ lug/worker-script/rsync-fetcher.sh
@@ -9,4 +9,4 @@ export RUST_BACKTRACE=1
 mkdir -p "${LUG_tmp_path}"
 
 timeout 4h /app/rsync-gc --s3-url "${LUG_s3_api}" --s3-region "${LUG_s3_region}" --s3-bucket "${LUG_s3_bucket}" --s3-prefix "rsync/${LUG_name}" --redis "${LUG_redis}" --redis-namespace "${LUG_name}" --keep "${LUG_keep}"
-timeout 4h /app/rsync-fetcher --src "${LUG_source}" --s3-url "${LUG_s3_api}" --s3-region "${LUG_s3_region}" --s3-bucket "${LUG_s3_bucket}" --s3-prefix "rsync/${LUG_name}" --redis "${LUG_redis}" --redis-namespace "${LUG_name}" --repository "${LUG_name}" --gateway-base "${LUG_gateway}/${LUG_name}" --tmp-path "${LUG_tmp_path}"
+timeout 4h /app/rsync-fetcher --src "${LUG_source}" --s3-url "${LUG_s3_api}" --s3-region "${LUG_s3_region}" --s3-bucket "${LUG_s3_bucket}" --s3-prefix "rsync/${LUG_name}" --redis "${LUG_redis}" --redis-namespace "${LUG_name}" --repository "${LUG_name}" --gateway-base "${LUG_gateway}/${LUG_name}" --tmp-path "${LUG_tmp_path}" ${LUG_rsync_extra_flags}
