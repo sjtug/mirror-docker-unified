c2bf1408b2861c67ce111de5a03742e6d17b977d
LightQuantum <self@lightquantum.me>
2023-03-11 01:59:40 +0800

    lug: fix bash escape for rsync-fetcher

--- lug/worker-script/rsync-fetcher.sh
+++ lug/worker-script/rsync-fetcher.sh
@@ -10,5 +10,5 @@ mkdir -p "${LUG_tmp_path}"
 
 LUG_timeout="${LUG_timeout:-4h}"
 
-timeout $LUG_timeout /app/rsync-gc --s3-url "${LUG_s3_api}" --s3-region "${LUG_s3_region}" --s3-bucket "${LUG_s3_bucket}" --s3-prefix "rsync/${LUG_name}" --redis "${LUG_redis}" --redis-namespace "${LUG_name}" --keep "${LUG_keep}"
-timeout $LUG_timeout /app/rsync-fetcher --src "${LUG_source}" --s3-url "${LUG_s3_api}" --s3-region "${LUG_s3_region}" --s3-bucket "${LUG_s3_bucket}" --s3-prefix "rsync/${LUG_name}" --redis "${LUG_redis}" --redis-namespace "${LUG_name}" --repository "${LUG_name}" --gateway-base "${LUG_gateway}/${LUG_name}" --tmp-path "${LUG_tmp_path}" ${LUG_rsync_extra_flags}
+eval timeout $LUG_timeout /app/rsync-gc --s3-url "\"${LUG_s3_api}\"" --s3-region "\"${LUG_s3_region}\"" --s3-bucket "\"${LUG_s3_bucket}\"" --s3-prefix "\"rsync/${LUG_name}\"" --redis "\"${LUG_redis}\"" --redis-namespace "\"${LUG_name}\"" --keep "\"${LUG_keep}\""
+eval timeout $LUG_timeout /app/rsync-fetcher --src "\"${LUG_source}\"" --s3-url "\"${LUG_s3_api}\"" --s3-region "\"${LUG_s3_region}\"" --s3-bucket "\"${LUG_s3_bucket}\"" --s3-prefix "\"rsync/${LUG_name}\"" --redis "\"${LUG_redis}\"" --redis-namespace "\"${LUG_name}\"" --repository "\"${LUG_name}\"" --gateway-base "\"${LUG_gateway}/${LUG_name}\"" --tmp-path "\"${LUG_tmp_path}\"" ${LUG_rsync_extra_flags}
