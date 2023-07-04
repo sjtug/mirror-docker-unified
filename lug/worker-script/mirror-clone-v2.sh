720006a539ea126b8dccaaece2746a6445c73712
LightQuantum <self@lightquantum.me>
2023-07-05 02:06:53 +0800

    fix: gcp query stuck due to no proxy

--- lug/worker-script/mirror-clone-v2.sh
+++ lug/worker-script/mirror-clone-v2.sh
@@ -4,4 +4,9 @@ set -e
 
 export $(cat /secrets_s3 | xargs)
 
+if [ "${LUG_use_proxy}" ]; then 
+    export http_proxy=http://clash:8080
+    export https_proxy=http://clash:8080
+fi
+
 timeout 4h /app/v2/mirror-clone $@
