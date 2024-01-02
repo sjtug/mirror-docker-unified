b3fa450ff144f62b7d1ee12ab156a98b8842957a
LightQuantum <self@lightquantum.me>
2024-01-01 18:35:44 -0800

    lug: fix archcn stuck after connection reset (#450)

--- lug/worker-script/zhiyuan/worker-script/archlinux-cn.sh
+++ lug/worker-script/zhiyuan/worker-script/archlinux-cn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /root/lug-secrets.sh
-RSYNC_PASSWORD=${ARCHLINUXCN_PASSWORD} rsync -rtlivH --delete-after --delay-updates --safe-links --max-delete=1000 --contimeout=60 ${ARCHLINUXCN_USERNAME}@sync.repo.archlinuxcn.org::repo $LUG_path
+RSYNC_PASSWORD=${ARCHLINUXCN_PASSWORD} rsync -rtlivH --delete-after --delay-updates --safe-links --max-delete=1000 --timeout=600 --contimeout=600 ${ARCHLINUXCN_USERNAME}@sync.repo.archlinuxcn.org::repo $LUG_path
