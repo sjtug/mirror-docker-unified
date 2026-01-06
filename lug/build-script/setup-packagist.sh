ef20e8bff6c63baacf43758a6bffd952896110cb
Yuxuan Sun <hjsdbb1@gmail.com>
2026-01-07 04:16:02 +0800

    lug: bump glibc version (#537)

--- lug/build-script/setup-packagist.sh
+++ lug/build-script/setup-packagist.sh
@@ -3,5 +3,7 @@ set -e
 
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-git clone https://github.com/sjtug/packagist-mirror --depth 1 && cd packagist-mirror
+wget https://github.com/sjtug/packagist-mirror/archive/refs/heads/master.tar.gz && \
+  tar -xf master.tar.gz && mv packagist-mirror-master packagist-mirror && rm master.tar.gz && \
+  cd packagist-mirror
 composer install
