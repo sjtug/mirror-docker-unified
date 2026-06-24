4c204661ba444dcb0eb003472d45df0bff9faaf3
comonad <comonad@a-stable.com>
2026-06-25 04:47:57 +0800

    chore: suppress curl progress for popos_dists.sh

--- scripts/popos_dists.sh
+++ scripts/popos_dists.sh
@@ -6,8 +6,8 @@ GREP=${GREP:-grep}
 
 function generate_dists() {
     module=$1
-    html=$(curl "https://apt-origin.pop-os.org/$1/dists/")
-    
+    html=$(curl -s "https://apt-origin.pop-os.org/$1/dists/")
+
     # Get the list of dists
     dists=($(echo "$html" | $GREP -oP '(?<=<a href=")[^"]*(?=/">)' | $GREP -v '\.\.'))
 }
