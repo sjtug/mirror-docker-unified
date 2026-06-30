c93088f69c12ec2a99ed3ece55f4328e92782b84
comonad <comonad@a-stable.com>
2026-07-01 01:58:35 +0800

    chore!: drop julia build-script

--- lug/build-script/setup-julia-mirror.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [ "$USE_SJTUG" = true ] ; then
-    export JULIA_PKG_SERVER=https://mirrors.ustc.edu.cn/julia
-fi
-
-julia -e 'using Pkg; pkg"add StorageMirrorServer@v0.2.1"'
